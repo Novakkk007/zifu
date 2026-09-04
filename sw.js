4105234c36496df14950936dc0b82e02875872b7
Hermes <hermes@zifu.dev>
2026-09-05 00:13:49 +0800

    deploy 2026-09-04 batch

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* 紫府最小 Service Worker：安装壳缓存（离线可开），运行时网络优先 */
 const SHELL = ['./', './manifest.json']
-const CACHE = 'zifu-shell-v1'
+const CACHE = 'zifu-shell-v2'
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()))
